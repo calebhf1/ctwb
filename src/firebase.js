76731324a16e07b9fae82c826697341088d15b2d
calebhf1 <62314190+calebhf1@users.noreply.github.com>
2026-05-28 11:22:39 -0500

    fix firebase config with env variables

--- src/firebase.js
+++ src/firebase.js
@@ -2,15 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getMessaging, getToken, onMessage } from "firebase/messaging";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 export const messaging = getMessaging(app);
-export const VAPID_KEY = "YOUR_VAPID_KEY";
+export const VAPID_KEY = process.env.REACT_APP_FIREBASE_VAPID_KEY;
 export { getToken, onMessage };
